b5d0fa996d4c660d7784100549cc734d70fed928
mark newman <mark.newman@pandora.net>
2016-04-21 11:37:22 -0400

    add in pipeline processing nuget update host config to use default config #2

--- Pandora.Azure.WebJobs.Extensions.TopicTrigger/HostConfigurationExtensions.cs
+++ Pandora.Azure.WebJobs.Extensions.TopicTrigger/HostConfigurationExtensions.cs
@@ -1,14 +1,11 @@
 ﻿// Copyright (c) PandoraJewelry. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
-using Microsoft.Azure.WebJobs.Host.Config;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.ServiceBus.Messaging;
-using Pandora.Azure.WebJobs.Extensions.TopicTrigger.Processors;
-using Pandora.Azure.WebJobs.Extensions.TopicTrigger.Filters;
+using Microsoft.Azure.WebJobs.Host.Config;
 using Pandora.Azure.WebJobs.Extensions.TopicTrigger.Binding;
+using System;
 
 namespace Pandora.Azure.WebJobs.Extensions.TopicTrigger
 {
@@ -16,20 +13,7 @@ namespace Pandora.Azure.WebJobs.Extensions.TopicTrigger
     {
         public static void UseTopicTriggers(this JobHostConfiguration config)
         {
-            var options = new OnMessageOptions()
-            {
-                MaxConcurrentCalls = 1,
-                AutoComplete = true
-            };
-            var topicconfig = new TopicConfiguration()
-            {
-                Processor = new MessageProcessor(options),
-                Filter = new EverySubscriptionFilter(),
-                ReScanInterval = 60*1000,
-                MessageOptions = options
-            };
-
-            UseTopicTriggers(config, topicconfig);
+            UseTopicTriggers(config, new TopicConfiguration());
         }
         public static void UseTopicTriggers(this JobHostConfiguration config, TopicConfiguration topicConfig)
         {

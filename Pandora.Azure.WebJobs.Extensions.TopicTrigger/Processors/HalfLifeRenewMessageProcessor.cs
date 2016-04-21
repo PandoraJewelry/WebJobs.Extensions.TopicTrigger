582a2235e4bd9016553a81ba59db64a6cc99d991
mark newman <mark.newman@pandora.net>
2016-04-21 11:59:09 -0400

    remove message processor in favor of using pipelineprocessor #2

--- Pandora.Azure.WebJobs.Extensions.TopicTrigger/Processors/HalfLifeRenewMessageProcessor.cs
+++ Pandora.Azure.WebJobs.Extensions.TopicTrigger/Processors/HalfLifeRenewMessageProcessor.cs
@@ -1,67 +1,28 @@
 ﻿// Copyright (c) PandoraJewelry. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Microsoft.Azure.WebJobs.Host.Executors;
-using Microsoft.ServiceBus.Messaging;
+
+using Pandora.Azure.WebJobs.PipelineCore;
+using Pandora.ServiceBus;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Pandora.Azure.WebJobs.Extensions.TopicTrigger.Processors
 {
-    public class HalfLifeRenewMessageProcessor : MessageProcessor, IMessageProcessor
+    public class HalfLifeRenewMessageProcessor : IMessageProcessor
     {
-        #region fields
-        private const string _renewToken = "RenewToken"; 
-        #endregion
-
-        #region constructors
-        public HalfLifeRenewMessageProcessor(OnMessageOptions messageOptions) : base(messageOptions) { } 
-        #endregion
-
-        public override Task<bool> BeginProcessingMessageAsync(BrokeredMessage message, CancellationToken cancellationToken)
+        public async Task Invoke(IPipelineContext context, Func<Task> next, CancellationToken cancellationToken)
         {
-            /// start auto renewing the message. useful when processing takes more than max service bus lock (5 min)
-            message.Properties[_renewToken] = AutoRenew(message);
-
-            return base.BeginProcessingMessageAsync(message, cancellationToken);
-        }
-        public override Task CompleteProcessingMessageAsync(BrokeredMessage message, FunctionResult result, CancellationToken cancellationToken)
-        {
-            /// stop the auto renew
-            (message.Properties[_renewToken] as Action)();
-
-            return base.CompleteProcessingMessageAsync(message, result, cancellationToken);
-        }
-
-        #region tools
-        private static Action AutoRenew(BrokeredMessage message)
-        {
-            var halflife = (int)((message.LockedUntilUtc - DateTime.Now.ToUniversalTime()).TotalMilliseconds / 2);
-            var token = new CancellationTokenSource();
-            var id = message.MessageId;
-
-            var task = Task.Run(async () =>
+            var done = context.Message.AutoRenew();
+            try
             {
-                while (true)
-                {
-                    try
-                    {
-                        await Task.Delay(halflife, token.Token);
-                        await message.RenewLockAsync();
-                    }
-                    catch (TaskCanceledException) { }
-                    if (token.IsCancellationRequested)
-                        break;
-                }
-            }, token.Token);
-
-            return () =>
+                await next();
+            }
+            finally
             {
-                token.Cancel();
-                task.Wait();
-            };
-        } 
-        #endregion
+                done();
+            }
+        }
     }
 }
--- Pandora.Azure.WebJobs.Extensions.TopicTrigger/TopicConfiguration.cs
+++ Pandora.Azure.WebJobs.Extensions.TopicTrigger/TopicConfiguration.cs
@@ -3,23 +3,31 @@
 
 using Microsoft.ServiceBus.Messaging;
 using Pandora.Azure.WebJobs.Extensions.TopicTrigger.Filters;
-using Pandora.Azure.WebJobs.Extensions.TopicTrigger.Processors;
+using Pandora.Azure.WebJobs.PipelineCore;
 
 namespace Pandora.Azure.WebJobs.Extensions.TopicTrigger
 {
     public class TopicConfiguration
     {
+        #region fields
         public OnMessageOptions MessageOptions { get; set; }
         public ISubscriptionFilter Filter { get; set; }
-        public IMessageProcessor Processor { get; set; }
+        public PipelineProcessor Processor { get; set; }
         public int ReScanInterval { get; set; }
+        #endregion
 
+        #region constructors
         public TopicConfiguration()
         {
-            MessageOptions = new OnMessageOptions();
+            MessageOptions = new OnMessageOptions()
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = true
+            };
             Filter = new EverySubscriptionFilter();
-            Processor = new MessageProcessor(MessageOptions);
-            ReScanInterval = 1;
+            Processor = new PipelineProcessor(MessageOptions);
+            ReScanInterval = 60 * 1000;
         }
+        #endregion
     }
 }
